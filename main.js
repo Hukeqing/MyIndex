356c5037b23a2d09c82a91ef334ff13de87800d6
Mauve <keqing.hu@icloud.com>
2019-09-03 09:10:21 +0800

    update and add the fastlinker count function

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-var num = 19;
+var num = 0;
+function cnt() {
+    var img_obj = document.getElementsByTagName("img")
+    for (var i = 0; i < img_obj.length; i++) {
+        if (img_obj.item(i).className == "radiuscenter") {
+            num++;
+        }
+    }
+}
 var lastSecond = new Date().getSeconds();
 window.onresize = function () {
     var minwidth = 70;
@@ -6,7 +14,7 @@ window.onresize = function () {
     var curwidth = Math.ceil(num / maxmum) * minwidth;
     document.getElementById('fastlinker').style.width = curwidth.toString() + "px";
 }
-function startresize () {
+function startresize() {
     var minwidth = 70;
     var maxmum = Math.floor(document.body.clientHeight / 90);
     var curwidth = Math.ceil(num / maxmum) * minwidth;
