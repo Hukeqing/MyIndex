e805f7b86945ba902805b4403578d4ac36d39dc8
HuKeqing <keqing.hu@icloud.com>
2020-03-30 16:10:29 +0800

    fix(cookie): fix the version bug on reload

--- js/background.js
+++ js/background.js
@@ -13,8 +13,8 @@ function changeBackground() {
     cur = dict[ans];
 }
 
-function initValue() {
-    getCookie();
+function initBG() {
+    initCookie();
     for (var i = 0; i < len; ++i) {
         if ((1 << i) & dislikeBG) continue;
         dict.push(i);
@@ -24,10 +24,10 @@ function initValue() {
 
 function addDislike() {
     if (dict.length == 1) {
-        window.alert("没有更多的壁纸啦！")
+        window.alert("没有更多的壁纸啦！");
     } else {
         dislikeBG |= 1 << cur;
-        setCookie();
+        setCookie('dislike', dislikeBG);
         var index = dict.indexOf(cur)
         dict.splice(index, 1);
         cur = -1;
@@ -37,7 +37,10 @@ function addDislike() {
 }
 
 function clearCookie() {
-    document.cookie = "";
+    var data = document.cookie.split(';');
+    for (var item in data) {
+        deleteCookie(data[item].split('=')[0]);
+    }
     window.location.reload();
 }
 
@@ -45,25 +48,35 @@ function downloadImg() {
     window.open('basic/background/' + cur + '.jpg');
 }
 
-function setCookie() {
-    document.cookie = 'version=' + version.toString() + ';dislike=' + dislikeBG.toString();
+function setCookie(name, value) {
+    var d = new Date();
+    d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000));
+    var expires = ";expires=" + d.toUTCString();
+    document.cookie = name + '=' + value + expires;
+}
+
+function deleteCookie(name) {
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
 }
 
 function logNewVersion() {
     window.alert("版本：1，更新公告：\n增加了添加不喜欢的壁纸功能，点击左下角的“dislike”按钮就可以添加当前壁纸哦。\n如果想要清空之前的选择，可以点击左下角的“clear”按钮就可以清空哦")
 }
 
-function getCookie() {
-    if (document.cookie != 0) {
-        data = document.cookie;
-        if (version != data[0].split('=')[1]) {
-            logNewVersion();
-            setCookie();
-        }
-        dislikeBG = data[1].split('=')[1];
-    } else {
-        logNewVersion();
-        setCookie();
-        dislikeBG = 0;
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
     }
+    return "";
+}
+
+function initCookie() {
+    if (getCookie('version') != version) {
+        logNewVersion();
+        setCookie('version', version);
+    }
+    dislikeBG = getCookie('dislike');
 }
--- js/main.js
+++ js/main.js
@@ -85,5 +85,5 @@ function init() {
     startClock();
     cnt();
     startresize();
-    initValue();
+    initBG();
 }
